b91013c79d230532502c39840bc62808aed1f0c3
Stefan Edwards <saedwards.ecc@gmail.com>
2017-03-28 09:50:53 -0400

    added logging instead of prints

--- timbothy.py
+++ timbothy.py
@@ -1,20 +1,32 @@
 from slackbot.bot import Bot, listen_to, respond_to, default_reply
 import re
 import json
+import logging
+
+
+LOG_FORMAT = '%(asctime)-15s %(message)s'
+logger = None
 
 
 @listen_to('#slack2tweet (.*) (.*)', re.IGNORECASE)
 def slack_to_tweet(message, description=None, url=None):
-    print "Recvieved: {0} {1}".format(description, url)
+    logging.info('received #slack2tweet: "{0}" {1}'.format(description, url))
     message.reply('... noted')
     message.react('+1')
 
 
 @respond_to('tweet (.*) (.*)', re.IGNORECASE)
 def tweet(message, description=None, url=None):
-    print "Recvieved: {0} {1}".format(description, url)
+    logging.info("Received direct tweet: {0} {1}".format(description, url))
     message.reply("... you're not my dad")
     message.react('+1')
+
+
+@respond_to('calcutron (.*)', re.IGNORECASE)
+@listen_to('calcutron (.*)', re.IGNORECASE)
+def calc(message, calc=None):
+    logging.info("I ain't your calculator.")
+    message.reply(':thinking_face:')
 
 
 @default_reply
@@ -23,5 +35,6 @@ def timbothy(message):
 
 
 if __name__ == '__main__':
+    logging.basicConfig(filename='slack.log', level=logging.INFO, format=LOG_FORMAT)
     bot = Bot()
     bot.run()
