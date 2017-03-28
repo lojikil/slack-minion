c37410e3579013a1984f35411311f8e474924111
Stefan Edwards <saedwards.ecc@gmail.com>
2017-03-28 12:13:07 -0400

    added Timmerish

--- timbothy.py
+++ timbothy.py
@@ -1,10 +1,19 @@
 from slackbot.bot import Bot, listen_to, respond_to, default_reply
 import re
 import logging
+import random
 
 
 LOG_FORMAT = '%(asctime)-15s %(message)s'
 logger = None
+commonwords = {
+    'the': ['teh', 'thr', 'thw'],
+    'this': ['thsi', 'ths', 'tis'],
+    'your': ['yor', 'you'],
+    "you're": "your",
+    "then": "than",
+    "i'm": "im",
+}
 
 
 @listen_to('#slack2tweet (.*) (.*)', re.IGNORECASE)
@@ -26,6 +35,42 @@ def tweet(message, description=None, url=None):
 def calc(message, calc=None):
     logging.info("I ain't your calculator.")
     message.reply(':thinking_face:')
+
+
+@respond_to('text2tim (.*)', re.IGNORECASE)
+@listen_to('text2tim (.*)', re.IGNORECASE)
+def text2tim(message, text):
+    text = text.replace(u'ay', u'e$y').replace(u'ey', u'a$y')
+    text = text.replace(u'ie', u'e$i').replace(u'ei', u'ie')
+    text = text.replace(u'$', u'')
+
+    words = text.lower().split(u' ')
+
+    results = []
+    print words
+    for word in words:
+        if word in commonwords:
+            if type(commonwords[word]) is list:
+                result = random.choice(commonwords[word])
+            else:
+                result = commonwords[word]
+
+            r = random.randint(0,100)
+
+            if r <= 50:
+                result = result.replace(u'is', u'si')
+                result = result.replace(u'ne', u'n')
+                result = result.replace(u'ss', u's')
+                result = result.replace(u'ed', u'd')
+                result = result.replace(u"'t", u"t")
+            elif r > 50 and r <= 75:
+                result = result.replace(u'ea', 'ae')
+
+            results.append(result)
+        else:
+            results.append(word)
+
+    message.reply(u' '.join(results))
 
 
 @default_reply
